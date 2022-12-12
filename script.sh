82bfefcfd5ecae639a5ef653f63de9e60f3a9d87
KFo3 <105438199+KFo3@users.noreply.github.com>
2022-12-12 13:07:15 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ while read -r net repo
 do
         ghe-spokesctl replica fail network/$net git-server-e9236588-4e22-11ed-b8da-005056b63f5d
         ghe-spokesctl bless $net/$repo git-server-41ff660e-bafd-11ea-9a74-005056a49c44
-done < repolistpartaf" >> scripttest.sh 
+done < repolistpartaf
