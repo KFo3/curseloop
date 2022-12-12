9b6c2137a9142f67f28ccd8831283aa73ee041bb
KFo3 <105438199+KFo3@users.noreply.github.com>
2022-12-12 13:40:17 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-echo "trap 'echo "The Curse and Bless process has completed!" ' EXIT
+trap 'echo "The Curse and Bless process has completed!" ' EXIT
 while read -r net repo
 do
         ghe-spokesctl replica fail network/$net git-server-e9236588-4e22-11ed-b8da-005056b63f5d
